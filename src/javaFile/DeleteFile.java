1e8ba326fc0a97b6f1042c313c37de033b5d7de5
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-05-06 16:17:06 +0200

    read file

---
+++ src/javaFile/DeleteFile.java
@@ -0,0 +1,2 @@
+package javaFile;public class DeleteFile {
+}
--- src/javaFile/ReadFile.java
+++ src/javaFile/ReadFile.java
@@ -1,20 +1,23 @@
 package javaFile;
 import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) {
         try {
             File myfile = new File("hello.txt");
-            if(myfile.createNewFile()){
-                System.out.println("fille created succesfully" + myfile.getName());
-            } else
-            {
-                System.out.println("file alredy exist");
+            Scanner myReader =  new Scanner(myfile);
+            while(myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                System.out.println(data);
             }
-        } catch (IOException e){
-            System.out.println("error in creating file");
+            myReader.close();
+
+        } catch (FileNotFoundException e){
+            System.out.println("error accurred");
             e.printStackTrace();
         }
     }
