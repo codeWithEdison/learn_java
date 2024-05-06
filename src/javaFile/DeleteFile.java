164a35f980ab9a23dbbfae258a20839d7d05bcce
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-05-06 16:17:20 +0200

    delete file

--- src/javaFile/DeleteFile.java
+++ src/javaFile/DeleteFile.java
@@ -1,2 +1,13 @@
-package javaFile;public class DeleteFile {
+package javaFile;
+import java.io.File;
+
+public class DeleteFile {
+    public static void main(String[] args) {
+       File myfile = new File("hello.txt");
+       if(myfile.delete()){
+           System.out.println("file deleted sucessfully  " +myfile.getName() );
+       } else {
+           System.out.println(" fail to delete file ");
+       }
+    }
 }
