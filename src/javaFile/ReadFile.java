4640bb6c276b88778531a4929a187b378b7b5bc6
Edison UWIHANGANYE <edsnkvn@gmail.com>
2024-05-06 16:01:23 +0200

    read file in java

--- src/javaFile/ReadFile.java
+++ src/javaFile/ReadFile.java
@@ -1,2 +1,21 @@
-package javaFile;public class ReadFile {
+package javaFile;
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.io.File;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File myfile = new File("hello.txt");
+            if(myfile.createNewFile()){
+                System.out.println("fille created succesfully" + myfile.getName());
+            } else
+            {
+                System.out.println("file alredy exist");
+            }
+        } catch (IOException e){
+            System.out.println("error in creating file");
+            e.printStackTrace();
+        }
+    }
 }
